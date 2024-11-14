f0a2fc98a9a63082b06f3cd86a575738f9adb096
Jaswant-3025 <jaswantkum3693@gmail.com>
2024-11-14 11:01:16 +0530

    First push

---
+++ dic.py
@@ -0,0 +1,22 @@
+# fruit ={
+#     "apple" :99,
+#     "banan": 50,
+#     "mango": 80}
+# name ={ "ggg":60,
+#        "hhhh":00}
+# fruit.update(name)
+# print(fruit)
+
+# fruit.popitem()
+# print(fruit)
+# "apple" in fruit
+
+
+# for i in fruit:
+#     print(i,fruit[i])
+
+# sets
+s ={"good","ehon"}
+s2 ={"rahul","good"} 
+a=s.union(s2)
+print(a)
